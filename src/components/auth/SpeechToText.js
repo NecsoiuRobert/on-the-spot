4519eeffa4a3866ece59fabb37a587050f8e627d
necmarius <necmarius23@gmail.com>
2020-11-01 03:41:29 +0200

    marius commit 2

--- src/components/auth/SpeechToText.js
+++ src/components/auth/SpeechToText.js
@@ -2,49 +2,23 @@ import React from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 let sayOnce = true;
 let finished = false;
-const SpeechToText = ({changeContent, timeSet,content}) => {
+const SpeechToText = ({changeContent, timeSet}) => {
   
   const { transcript, resetTranscript } = useSpeechRecognition()
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null
   }
-  const synthRef = window.speechSynthesis;
- const correct = (text) => {
-    const utterThis = new SpeechSynthesisUtterance(text);
-    if(utterThis && synthRef){
-        utterThis.voice = synthRef.getVoices()[0];
-        utterThis.rate = 1.5;
-        utterThis.lang = "en-US";
-        synthRef.speak(utterThis);
-    }
-};
 
-  if(sayOnce){
-    correct('If you are unable to see say blind');
-    sayOnce = false;
-   }
-if(!finished){
-  SpeechRecognition.startListening({continous:true});
+  const onClickHandler = () => {
+    SpeechRecognition.stopListening();
+    changeContent(transcript);
+  }
 
-setTimeout(() => {
-  SpeechRecognition.stopListening()
-  
-  changeContent(transcript);
-},timeSet);
-
-finished = true;
-}
-
-  // const onClickHandle = () =>{
-  //   SpeechRecognition.stopListening()
-  //   changeContent(transcript);
-  // }
   return (
     <div>
-      {/* <button onClick={onClickHandle}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-    */}
+      <button type="button" class="btn btn-primary" onClick={SpeechRecognition.startListening({continous:true})}>Start</button>
+      <button type="button" class="btn btn-primary" onClick={onClickHandler}>Stop</button>
     </div>
   )
 }
